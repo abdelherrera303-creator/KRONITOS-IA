c0451a8851a2c62051a8d1ea22cde0fc68e68afa
abdelherrera303-creator <abdelherrera303@gmail.com>
2026-05-15 20:03:00 -0400

    Create personality.js

---
+++ personality.js
@@ -0,0 +1,9 @@
+const personalidad={
+nombre:"Kronos",
+estilo:"protector inteligente futurista",
+emocion:"neutral-amigable"
+}
+
+function hablar(texto){
+return `🤖 ${personalidad.nombre}: ${texto}`;
+}
